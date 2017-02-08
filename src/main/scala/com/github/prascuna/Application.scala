ee6910119338fbdb1753d2cf3ee546b0848f5c87
Paolo Rascuna <prascuna@gmail.com>
2017-02-08 17:37:11 +0000

    main app

--- src/main/scala/com/github/prascuna/Application.scala
+++ src/main/scala/com/github/prascuna/Application.scala
@@ -3,25 +3,60 @@ package com.github.prascuna
 import java.io.File
 import java.time.format.DateTimeFormatter
 
-import com.github.prascuna.services.InputLoaderImpl
+import com.github.prascuna.models.AddressBook.GenderEnum.Male
+import com.github.prascuna.services.{AnswersServiceImpl, InputLoaderImpl}
 
 import scala.util.{Failure, Success}
 
 object Application {
 
   def main(args: Array[String]): Unit = {
-    val file = new File(args(0))
-    val Columns = 3 // in a real app this would be taken from configuration
+    if(args.size != 1) {
+      showUsage
+    } else {
+      val file = new File(args(0))
+      val Columns = 3 // in a real app this would be taken from configuration
 
-    val dtf = DateTimeFormatter.ofPattern("dd/MM/yy")
-    val loader = new InputLoaderImpl(dtf, Columns)
+      val dtf = DateTimeFormatter.ofPattern("dd/MM/yy")
+      val loader = new InputLoaderImpl(dtf, Columns)
 
-    loader.load(file) match {
-      case Success(addressBook) =>
+      loader.load(file) match {
+        case Success(addressBook) =>
+          val answersService = new AnswersServiceImpl(addressBook)
 
-      case Failure(_) =>
-        println("Cannot load AddressBook")
+          val noOfMales = answersService.countByGender(Male)
+
+          val oldestPerson = answersService.oldestPerson()
+
+          val daysDifference = answersService.ageDifference("Bill McKnight", "Paul Robinson")
+
+          val answers = List(
+            printableAnswer(1, noOfMales.toString),
+            printableAnswer(2, oldestPerson.name),
+            daysDifference match {
+              case Success(days) =>
+                printableAnswer(3, days.toString)
+              case Failure(e) =>
+                printableAnswer(3, "Error: " + e.getMessage)
+            }
+          )
+
+          answers.foreach(println)
+
+        case Failure(_) =>
+          println("Cannot load AddressBook")
+      }
     }
   }
+
+  private def printableAnswer(questionNumber: Int, answer: String) = s"$questionNumber. $answer"
+
+  private def showUsage = println(
+    """No such file or directory
+      |Please run the application with the following command:
+      |
+      |sbt "run <filename>"
+    """.stripMargin)
+
 
 }
